e51b31103e8581b11bce41844cabfdc0c38b4544
Blackflame6 <engineerfranklyn@gmail.com>
2022-10-09 09:46:02 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res)=>{
+  res.status(200).send({
+    success: true,
+    message: "Node deployment"
+  })
+})
+
+
+let PORT = 3000;
+
+app.listen(PORT, ()=>{
+  console.log(`App is running on port ${PORT}`)
+})
